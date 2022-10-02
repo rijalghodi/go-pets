98c9d94af2c2a8f4cd65c9d31e3300f051b0ce8e
Rijal Ghodi <rijalghodi10@gmail.com>
2022-10-02 11:43:21 +0700

    add first version of app

---
+++ src/components/hero/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Hero = ({ image, type }) => {
+  return (
+    <div
+      className="hero-container"
+      style={{
+        backgroundImage: `linear-gradient(black, black), url("${
+          image || 'pets-hero.png'
+        }")
+          `,
+        backgroundBlendMode: 'saturation',
+        backgroundSize: 'cover',
+        backgroundColor: '#0000008f'
+      }}
+    >
+      <h2>{getHeroTitle(type)}</h2>
+    </div>
+  );
+};
+
+export default Hero;
+
+const getHeroTitle = (type) => {
+  switch (type) {
+    case 'dog':
+      return 'Dogs';
+    case 'cat':
+      return 'Cats';
+    case 'rabbit':
+      return 'Rabbits';
+    case 'bird':
+      return 'Birds';
+    default:
+      return 'Find your perfect pet';
+  }
+};
